2d40861498cbb0158cc16e154cba4e0bc7cd3c2b
Naysa Moreno <naysaymoreno@gmail.com>
2021-03-29 14:34:19 -0500

    added break and continue

---
+++ break_and_continue.js
@@ -0,0 +1,19 @@
+"use strict"
+
+
+var userInput = 0
+while (userInput !== "you are never gonna get this") {
+    userInput = parseFloat(prompt("Enter an odd number between 1- 50"));
+    if (userInput %2 === 1 || (userInput > 0 && userInput < 50)) {break;}
+}
+// while (userInput % 2 !== 1 || userInput < 1 || userInput < 50 )  {
+//     userInput = parseFloat(prompt("Enter an odd number between 1- 50"));
+//     if (userInput % 2 === 1){break;}
+// }
+for (var  i = 1; i <50; i+=2) {
+    if (i === userInput) {
+        console.log("yikes, gonna skip " + userInput);
+        continue;
+    }
+    console.log(i);
+}

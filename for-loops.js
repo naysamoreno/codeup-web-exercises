021f6fe8082a7f81b5d1a21f5833c744f96a393c
Naysa Moreno <naysaymoreno@gmail.com>
2021-03-29 12:24:32 -0500

    for loops

---
+++ for-loops.js
@@ -0,0 +1,51 @@
+"use strict"
+
+// var number = 7;
+// function showMultiplicationTable() {
+//     for(var multiplier = 1; multiplier < 10; multiplier++) {
+//         console.log(number + " x " + multiplier + " = " + (number * multiplier));
+//     }
+// }
+// showMultiplicationTable(7);
+// for (var i=0; i<10; i++) {
+//     var randNum = Math.floor(Math.random() * 181 + 20);
+//     if (randNum % 2 === 0) {
+//         console.log(randNum + " its Even!");
+//     }
+//     else {
+//         console.log(randNum + " its odd!")
+//     }
+// }
+// fancy way
+// function randomIntFromInterval(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+// var random, evenOddString;
+// for (var i = 0; i < 10; i++) {
+//     random = randomIntFromInterval(20, 200);
+//     evenOddString = (random % 2 === 0) ? "even" : "odd";
+//     console.log(random + " is " + evenOddString);
+// }
+// for (var i=0; i<9; i++) {
+//     var increaseNum = ('');
+//     for (var n = 0; n <= i; n++) {
+//         increaseNum = increaseNum + (i + 1) + "";
+//     }
+//     console.log(increaseNum);
+// }
+// VERY FANCY
+// function range(start = 0, end) {
+//     const num = (end + 1) - start
+//     return new Array(num).fill().map((_, index) => start + index);
+// }
+//
+// const NUM = 9;
+// const RANGE = range(1, NUM);
+// for(var num of RANGE) {
+//     var numberString = new Array(num).fill().map(() => ''+num).join('');
+//     console.log(numberString);
+// }
+
+for (var i = 100; i > 0;i = i - 5) {
+    console.log(i)
+}
---
+++ js/while.js
@@ -0,0 +1,29 @@
+// var counter = 2;
+// while (counter <= 65536){
+//     console.log(counter);
+//     counter = counter * 2;
+// }
+// var allCones = Math.floor(Math.random() * 50) + 50;
+//
+// do {
+//     var conesBought = Math.floor(Math.random() * 5) + 1;
+//     if (conesBought > allCones) {
+//         console.log("I cant sell " + conesBought + " I only have  " + allCones + " left sorry");
+//     } else {
+//         allCones = allCones - conesBought;
+//         console.log("Cones Sold " + conesBought + " I have " + allCones + " left to sell");
+//     }
+// } while (allCones > 0);
+
+// while (allCones > 0) {
+//     var conesBought = Math.floor(Math.random() * 5) + 1;
+//     if (conesBought > allCones) {
+//         console.log("I cant sell " + conesBought + " I only have  " + allCones + " left sorry");
+//     } else {
+//         allCones = allCones - conesBought;
+//         console.log("Cones Sold " + conesBought + " I have " + allCones + " left to sell");
+//     }
+// }
+
+// console.log("I sold all ma cones")
+

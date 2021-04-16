ee2dbd137fd9be1c4a6365604ed3f94846cb1e7c
Naysa Moreno <naysaymoreno@gmail.com>
2021-04-16 11:31:16 -0500

    media-queries-lecture

---
+++ practice-for-test/JS-study-guide-2.js
@@ -0,0 +1,49 @@
+// # JavaScript Loops, Arrays, and Objects Assessment
+// ## Main Concepts to be Assessed
+// - JavaScript Primitive Types
+function isBoolean(input){
+    return Boolean(input);
+}
+console.log(isBoolean(true));
+console.log(isBoolean("false"));
+console.log(isBoolean(51));
+console.log(isBoolean(-1));
+
+// - JavaScript Operators
+// - JavaScript Variables
+// - JavaScript Functions
+// - JavaScript Conditionals
+// - JavaScript Loops
+// - JavaScript Arrays
+// - Manipulating JavaScript Arrays
+// - JavaScript Objects
+// - The Math Object in JavaScript
+// - Writing Code That Passes Pre-written Tests in Jasmine.
+//     ## Skills Not Currently Being Assessed
+// - Writing Tests in Jasmine
+// - Browser Object Model
+// - Document Object Model
+// ## JavaScript Loops, Arrays, and Objects Checklist
+// The following are several points of competency and skill that are intended to help identify areas of additional practice and study. Not all answers are found in the Codeup curriculum.
+//     ### Knowledge
+// In addition to the concepts from the previous assessment,
+//     - [ ] I can differentiate between the various loops of JavaScript.
+// - [ ] I can identify the use case for break and continue in loops.
+// - [ ] I can identify an array in JavaScript.
+// - [ ] I know how to iterate through an array in JavaScript.
+// - [ ] I know the various methods that can be used to manipulate an array.
+// - [ ] I know the various parts of an object in JavaScript.
+// - [ ] I have familiarized myself with the Math object, and its various methods and properties.
+//     ### Skills
+// In addition to the skills from the previous assessment,
+//     - [ ] I can write code in a looping structure based on a condition.
+// - [ ] I can write code that will stop a loop.
+// - [ ] I can create an array in JavaScript.
+// - [ ] I can access a single piece of information from an array in JavaScript.
+// - [ ] I can navigate through an array, and access all piece of information individually.
+// - [ ] I can add and remove information from an array.
+// - [ ] I can change the order of information in an array.
+// - [ ] I can convert between an array and string in JavaScript.
+// - [ ] I can create and use objects in JavaScript.
+// - [ ] I can add methods and properties to objects in JavaScript.
+// - [ ] I know how to use the methods and properties from an object in JavaScript.

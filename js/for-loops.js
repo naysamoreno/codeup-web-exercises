06ddde658ee66851c79b043270894201e823850f
Naysa Moreno <naysaymoreno@gmail.com>
2021-04-27 11:19:32 -0500

    used the loops html and js page for practice on next exam

--- js/for-loops.js
+++ js/for-loops.js
@@ -1,21 +1,21 @@
 "use strict"
 
-// var number = 7;
-// function showMultiplicationTable() {
-//     for(var multiplier = 1; multiplier < 10; multiplier++) {
-//         console.log(number + " x " + multiplier + " = " + (number * multiplier));
-//     }
-// }
-// showMultiplicationTable(7);
-// for (var i=0; i<10; i++) {
-//     var randNum = Math.floor(Math.random() * 181 + 20);
-//     if (randNum % 2 === 0) {
-//         console.log(randNum + " its Even!");
-//     }
-//     else {
-//         console.log(randNum + " its odd!")
-//     }
-// }
+var number = 7;
+function showMultiplicationTable() {
+    for(var multiplier = 1; multiplier < 10; multiplier++) {
+        console.log(number + " x " + multiplier + " = " + (number * multiplier));
+    }
+}
+showMultiplicationTable(7);
+for (var i=0; i<10; i++) {
+    var randNum = Math.floor(Math.random() * 181 + 20);
+    if (randNum % 2 === 0) {
+        console.log(randNum + " its Even!");
+    }
+    else {
+        console.log(randNum + " its odd!")
+    }
+}
 // fancy way
 // function randomIntFromInterval(min, max) {
 //     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -45,7 +45,32 @@
 //     var numberString = new Array(num).fill().map(() => ''+num).join('');
 //     console.log(numberString);
 // }
+// function sayHello () {
+//     alert("hello world!");
+// }
+// sayHello();
 
-for (var i = 100; i > 0;i = i - 5) {
-    console.log(i)
+// for (var i = 100; i > 0;i = i - 5) {
+//     console.log(i)
+// }
+
+// 1
+// 22
+// 333
+// 4444
+// 55555
+// 666666
+// 7777777
+// 88888888
+// 999999999
+for (var i = 1; i <=9; i++) {
+    let str = ""
+    for (var j = 1; j <= i; j++) {
+        str += i
+    }
+    console.log(str)
 }
+//
+/*
+
+ */

81cc4bef44e211e4e2bf32e7408d131839cf165a
Naysa Moreno <naysaymoreno@gmail.com>
2021-06-01 23:20:27 -0500

    corrected error in functions.js

--- js/warm-up.js
+++ js/warm-up.js
@@ -2,57 +2,48 @@
 //     if (input % 3 === 0  && input % 5 === 0) {
 //          return console.log("FizzBuzz")
 //     } else if (input % 3 === 0) {
-<<<<<<< HEAD
-//         return "Fizz"
-//     } else if (input % 5 === 0) {
-//         return "buzz"
-//     } else {return input}
-=======
 //         return console.log("Fizz")
 //     } else if (input % 5 === 0) {
 //         return console.log("buzz")
 //     } else {return console.log(input)}
->>>>>>> f6e02d7 (fizzBuzz warmup)
 // }
-(() => {
-    const fizzBuzz = numTil => {
-        for (let x = 1; x <= numTil; x++) {
-            if (x % 3 === 0 && x % 5 === 0) {
-<<<<<<< HEAD
-                 console.log("FizzBuzz")
-            } else if (x % 3 === 0) {
-                 console.log("Fizz")
-            } else if (x % 5 === 0) {
-                 console.log("buzz")
-            } else {
-                 console.log (x)
-            }
-        }
+// (() => {
+//     const fizzBuzz = numTil => {
+//         for (let x = 1; x <= numTil; x++) {
+//             if (x % 3 === 0 && x % 5 === 0) {
+//                 console.log("FizzBuzz")
+//             } else if (x % 3 === 0) {
+//                 console.log("Fizz")
+//             } else if (x % 5 === 0) {
+//                 console.log("buzz")
+//             } else {
+//                 console.log (x)
+//             }
+//         }
+//     }
+//     fizzBuzz(30);
+// })
+//
+// function seven (input) {
+//     if (typeof input === "string" || typeof input === "number") {
+//         return console.log(7)
+//     } else
+//     return console.log(7)
+// }
+// seven('boop');
+//
+// //Davids answer
+//
+// const returnSeven = () => 7;
+function timesFive(input) {
+    if (typeof input === "number") {
+        return input * 5;
+    } else {
+        return 0;
     }
- fizzBuzz(30);
-=======
-                console.log("FizzBuzz")
-            } else if (x % 3 === 0) {
-                console.log("Fizz")
-            } else if (x % 5 === 0) {
-                console.log("buzz")
-            } else {
-                console.log (x)
-            }
-        }
-    }
-    fizzBuzz(30);
->>>>>>> f6e02d7 (fizzBuzz warmup)
-})
-
-function seven (input) {
-    if (typeof input === "string" || typeof input === "number") {
-        return console.log(7)
-    } else
-    return console.log(7)
 }
-seven('boop');
+// ES6
+const x5 = (input) =>  typeof input === "number" ? input*5 : 0
 
-//Davids answer
-
-const returnSeven = () => 7;
+console.log(timesFive(7))
+console.log(timesFive("hello"))
